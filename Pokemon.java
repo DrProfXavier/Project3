04c5d8c587be39a22293ac51b6496e236fe64843
Xavier Esteve <xesteve@ufl.edu>
2017-11-03 12:58:15 -0400

    Add files via upload

---
+++ Pokemon.java
@@ -0,0 +1,69 @@
+public class Pokemon
+{
+    protected int attack, defense, speed;
+    protected String species;
+
+
+    public Pokemon()
+    {
+        species = "";
+        attack = species.length() * 4 + 2;
+        defense = species.length() * 2 + 7;
+        speed = species.length() * 3 + 5;
+
+    }
+
+    public void setAttack(int newAttack)
+    {
+
+
+    }
+
+    public int getAttack()
+    {
+
+        return;
+    }
+
+    public void setDefense(int newDefense)
+    {
+
+
+    }
+
+    public int getDefense()
+    {
+
+        return;
+    }
+
+    public void setSpeed(int newSpeed)
+    {
+
+
+    }
+
+    public int getSpeed()
+    {
+
+        return;
+    }
+
+    public String setSpecies()
+    {
+
+        return;
+    }
+
+    public void getSpecies(String newSpecies)
+    {
+
+    }
+
+    public void evolve()
+    {
+
+    }
+
+
+}
